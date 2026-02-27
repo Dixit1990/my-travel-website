bc8b58e27da0615f0ba0c4a416069aaa9df62f0f
Dixit1990 <dixitsagar786@gmail.com>
2026-02-27 23:58:17 +0530

    trip 1 img name change

--- app.py
+++ app.py
@@ -42,14 +42,14 @@ trips = [
             "Travel tip: Rent a scooter to explore hidden spots."
         ],
         "photos": [
-            "images/trip2/photo1.jpg",
-            "images/trip2/photo2.jpg",
-            "images/trip2/photo3.jpg",
-            "images/trip2/photo4.jpg",
-            "images/trip2/photo5.jpg",
-            "images/trip2/photo6.jpg",
-            "images/trip2/photo7.jpg",
-            "images/trip2/photo8.jpg"
+            "images/trip2/photo-1.jpg",
+            "images/trip2/photo-2.jpg",
+            "images/trip2/photo-3.jpg",
+            "images/trip2/photo-4.jpg",
+            "images/trip2/photo-5.jpg",
+            "images/trip2/photo-6.jpg",
+            "images/trip2/photo-7.jpg",
+            "images/trip2/photo-8.jpg"
         ],
         "map_embed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d502958.13718854846!2d75.97243909913463!3d9.987054981985183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d514abec6bf%3A0xbd582caa5844192!2sKochi%2C%20Kerala!5e0!3m2!1sen!2sin!4v1772015602673!5m2!1sen!2sin"
     },
@@ -67,14 +67,14 @@ trips = [
             "Travel tip: Pack warm clothes even in summer!"
         ],
         "photos": [
-            "images/trip3/photo1.jpg",
-            "images/trip3/photo2.jpg",
-            "images/trip3/photo3.jpg",
-            "images/trip3/photo4.jpg",
-            "images/trip3/photo5.jpg",
-            "images/trip3/photo6.jpg",
-            "images/trip3/photo7.jpg",
-            "images/trip3/photo8.jpg"
+            "images/trip3/photo-1.jpg",
+            "images/trip3/photo-2.jpg",
+            "images/trip3/photo-3.jpg",
+            "images/trip3/photo-4.jpg",
+            "images/trip3/photo-5.jpg",
+            "images/trip3/photo-6.jpg",
+            "images/trip3/photo-7.jpg",
+            "images/trip3/photo-8.jpg"
         ],
         "map_embed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62821.0367601578!2d77.45712016777094!3d10.236174976988446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0766637f0a0387%3A0x9ffae9373758c13c!2sKodaikanal%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1772015666474!5m2!1sen!2sin"
     }
